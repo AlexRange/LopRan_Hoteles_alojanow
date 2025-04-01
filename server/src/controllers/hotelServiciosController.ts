3b88ff84b886d5716e343b53c2d7baf837edb237
Rl435701 <116208829+Rl435701@users.noreply.github.com>
2025-04-01 17:42:17 -0600

    correciones

--- server/src/controllers/hotelServiciosController.ts
+++ server/src/controllers/hotelServiciosController.ts
@@ -6,16 +6,15 @@ class HotelServiciosController {
     public async getServiciosByHotel(req: Request, res: Response): Promise<void> {
         const { id_hotel } = req.params;
         try {
-            const servicios = await pool.then(poolInstance => 
-                poolInstance.query(`
-                    SELECT s.*
-                    FROM servicios_adicionales s
-                    INNER JOIN hotel_servicios hs ON s.id_servicio = hs.id_servicio
-                    WHERE hs.id_hotel = ?
-                `, [id_hotel])
-            );
-            if (servicios.length > 0) {
-                res.json(servicios);
+            const servicios = await pool.query(`
+                SELECT s.*
+                FROM servicios_adicionales s
+                INNER JOIN hotel_servicios hs ON s.id_servicio = hs.id_servicio
+                WHERE hs.id_hotel = ?
+            `, [id_hotel]);
+
+            if (servicios && servicios[0]) {
+                res.json(servicios[0]);
             } else {
                 res.status(404).json({ message: "No services found for this hotel" });
             }
@@ -29,10 +28,11 @@ class HotelServiciosController {
     public async addServicioToHotel(req: Request, res: Response): Promise<void> {
         const { id_hotel, id_servicio } = req.params;
         try {
-            const result = await pool.then(poolInstance => 
-                poolInstance.query('INSERT INTO hotel_servicios (id_hotel, id_servicio) VALUES (?, ?)', [id_hotel, id_servicio])
+            await pool.query(
+                'INSERT INTO hotel_servicios (id_hotel, id_servicio) VALUES (?, ?)', 
+                [id_hotel, id_servicio]
             );
-            res.json({ message: 'Service added to hotel', id_hotel, id_servicio });
+            res.json({ message: 'Service added to hotel' });
         } catch (error) {
             console.error('Error adding service to hotel:', error);
             res.status(500).json({ message: 'Error adding service to hotel' });
@@ -43,14 +43,11 @@ class HotelServiciosController {
     public async removeServicioFromHotel(req: Request, res: Response): Promise<void> {
         const { id_hotel, id_servicio } = req.params;
         try {
-            const result = await pool.then(poolInstance => 
-                poolInstance.query('DELETE FROM hotel_servicios WHERE id_hotel = ? AND id_servicio = ?', [id_hotel, id_servicio])
+            await pool.query(
+                'DELETE FROM hotel_servicios WHERE id_hotel = ? AND id_servicio = ?', 
+                [id_hotel, id_servicio]
             );
-            if (result.affectedRows > 0) {
-                res.json({ message: 'Service removed from hotel' });
-            } else {
-                res.status(404).json({ message: "Service not found for this hotel" });
-            }
+            res.json({ message: 'Service removed from hotel' });
         } catch (error) {
             console.error('Error removing service from hotel:', error);
             res.status(500).json({ message: 'Error removing service from hotel' });
--- server/src/database.ts
+++ server/src/database.ts
@@ -1,32 +1,32 @@
-import mysql from 'promise-mysql';
-import keys from './keys';
-
-const poolPromise = mysql.createPool(keys.database);
-
-poolPromise.then(pool => {
-    return pool.getConnection()
-        .then(connection => {
-            connection.release();
-            console.log('DB is Connected to Clever Cloud');
-        });
-}).catch(err => {
-    console.error('Error connecting to the database:', err);
-});
-
-export default poolPromise;
-
-// import mysql from 'mysql2/promise';
+// import mysql from 'promise-mysql';
 // import keys from './keys';
 
 // const poolPromise = mysql.createPool(keys.database);
 
-// poolPromise.getConnection()
-//     .then(connection => {
-//         connection.release();
-//         console.log('DB is Connected to Clever Cloud');
-//     })
-//     .catch(err => {
-//         console.error('Error connecting to the database:', err);
-//     });
+// poolPromise.then(pool => {
+//     return pool.getConnection()
+//         .then(connection => {
+//             connection.release();
+//             console.log('DB is Connected to Clever Cloud');
+//         });
+// }).catch(err => {
+//     console.error('Error connecting to the database:', err);
+// });
 
 // export default poolPromise;
+
+import mysql from 'mysql2/promise';
+import keys from './keys';
+
+const poolPromise = mysql.createPool(keys.database);
+
+poolPromise.getConnection()
+    .then(connection => {
+        connection.release();
+        console.log('DB is Connected to Clever Cloud');
+    })
+    .catch(err => {
+        console.error('Error connecting to the database:', err);
+    });
+
+export default poolPromise;
