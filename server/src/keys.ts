b20eb4a439196def310843b4912410376305199f
Rl435701 <116208829+Rl435701@users.noreply.github.com>
2025-04-01 17:35:57 -0600

    o^o

--- server/src/keys.ts
+++ server/src/keys.ts
@@ -8,11 +8,16 @@
 
 export default {
     database: {
-        host: 'bva3uonljeeexarutwta-mysql.services.clever-cloud.com',
-        user: 'uv4y0jdvnq6o6i25',
-        password: '4SmpVJk2TSd4gXOZMw8h',
-        database: 'bva3uonljeeexarutwta',
-        port: 3306,
-        connectionLimit: 5
+        host: process.env.MYSQL_ADDON_HOST || 'bva3uonljeeexarutwta-mysql.services.clever-cloud.com',
+        user: process.env.MYSQL_ADDON_USER || 'uv4y0jdvnq6o6i25',
+        password: process.env.MYSQL_ADDON_PASSWORD || '4SmpVJk2TSd4gXOZMw8h',
+        database: process.env.MYSQL_ADDON_DB || 'bva3uonljeeexarutwta',
+        port: Number(process.env.MYSQL_ADDON_PORT) || 10000,
+        ssl: {
+            rejectUnauthorized: false // Obligatorio para Clever Cloud
+        },
+        waitForConnections: true,
+        connectionLimit: 10,
+        namedPlaceholders: true
     }
 };
